c6eb578c0a92cf16ac25593c6d2842f8b874ee74
Margaret31-lab <marg.miller.2007@gmail.com>
2026-02-12 17:04:57 +0300

    Add files via upload

---
+++ Task2.py/task_2-0_5.py
@@ -0,0 +1,4 @@
+name="Вова"
+group=3
+score=25
+print(f"Студент {name} из группы {group} получил {score} баллов")
---
+++ Task2.py/Тask2-06.py
@@ -0,0 +1,3 @@
+f = open("output.txt", "w", encoding="utf-8")
+print("Поступление 2025, группа 4731901", file=f)
+f.close()
