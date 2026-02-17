62ea134c4e89e27bf0577ffd887db77ae6c1711d
Margaret31-lab <marg.miller.2007@gmail.com>
2026-02-17 23:10:37 +0300

    Add files via upload

---
+++ Task2.py/projects_2/task_2_2/task_2-2_1.py
@@ -0,0 +1,4 @@
+course_name = "Информатика"
+stage_number = 2
+task_completed = 21
+print(f"Course {course_name}, stage {stage_number}, done {task_completed}")
---
+++ Task2.py/projects_2/task_2_2/task_2-2_3.py
@@ -0,0 +1,7 @@
+device_name = "Биореактор BioFlo 120"
+inventory_number = "BR-120-45"
+is_operational = True
+quantity = 1
+
+print("Название прибора\tИнв.номер\tCостояние\tКоличество")
+print(f"{device_name}\t{inventory_number}\t{is_operational}\t{quantity}")
